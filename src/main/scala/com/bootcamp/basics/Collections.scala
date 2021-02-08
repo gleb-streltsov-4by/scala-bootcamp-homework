befb6349e1d43080aa7aa81c20a7119afc60b857
Hleb <gleb.streltsov.4by@gmail.com>
2021-02-08 10:37:36 +0300

    Add homework 4

--- src/main/scala/com/bootcamp/basics/Collections.scala
+++ src/main/scala/com/bootcamp/basics/Collections.scala
@@ -4,18 +4,16 @@ import scala.annotation.tailrec
 
 object Collections {
 
-  def runningSum(ls: List[Int]): List[Int] = ls match {
+  // https://leetcode.com/problems/shuffle-the-array
+  def shuffle[T](ls: List[T]): List[T] = ls match {
     case Nil => Nil
-    case h :: t => t.scanLeft(h)(_ + _)
+    case _ =>
+      val len = ls.length / 2
+      (ls.take(len) zip ls.takeRight(len))
+        .flatMap { case (x, y) => List(x, y) }
   }
 
-  def shuffle[T](ls: List[T]): List[T] = {
-    ls match {
-      case Nil => Nil
-      case _ => Nil
-    }
-  }
-
+  // https://leetcode.com/problems/richest-customer-wealth
   def richest(matrix: List[List[Int]]): Option[Int] = {
     def maxWithOpt(a: Int, opt: Option[Int]): Option[Int] = {
       (a, opt) match {
@@ -31,6 +29,44 @@ object Collections {
       case _ => helper(acc, matrix.tail)
     }
     helper(None, matrix)
+  }
+
+  // https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+  def greatest(candies: List[Int], extraCandies: Int): List[Boolean] = candies match {
+    case Nil  => Nil
+    case _    =>
+      val max = candies.max
+      candies.map(c => c + extraCandies >= max)
+  }
+
+  // https://leetcode.com/problems/widest-vertical-area-between-two-points-containing-no-points/
+  def widest(points: List[(Int, Int)]): Option[Int] = {
+    @tailrec
+    def helper(acc: Option[Int], xs: List[Int]): Option[Int] = xs match {
+      case a :: b :: t => helper(
+        if (b - a > acc.getOrElse(Int.MinValue)) Some(b - a) else acc,
+        b :: t)
+      case _ => acc
+    }
+
+    points match {
+      case Nil  => None
+      case _    =>
+        val xs = points
+          .map { case (x, _) => x }
+          .sorted
+
+        helper(None, xs) match {
+          case Some(0)  => None
+          case v        => v
+        }
+    }
+  }
+
+  // https://leetcode.com/problems/running-sum-of-1d-array/
+  def runningSum(ls: List[Int]): List[Int] = ls match {
+    case Nil => Nil
+    case h :: t => t.scanLeft(h)(_ + _)
   }
 }
 
@@ -75,7 +111,10 @@ object LinkedList extends App {
 
   // https://twitter.com/allenholub/status/1357115515672555520/photo/1
   // pass the interview
-  def count(s: String): List[(Char, Int)] = {
-    ???
+  def count(s: String): List[(Char, Int)] = s match {
+    case _ if s.isEmpty => Nil
+    case _ =>
+      val prefix = s.takeWhile(c => c == s.head)
+      s.head -> prefix.length :: count(s.drop(prefix.length))
   }
 }
--- src/test/scala/com/bootcamp/basics/CollectionsSpec.scala
+++ src/test/scala/com/bootcamp/basics/CollectionsSpec.scala
@@ -24,12 +24,12 @@ class CollectionsSpec extends AnyFlatSpec with ScalaCheckDrivenPropertyChecks {
     runningSum(input) shouldEqual output
   }
 
-//  "shuffle" should "return correct results" in {
-//    val input = List(1,2,3,4,5,6)
-//    val output = List(1,4,2,5,3,6)
-//
-//    shuffle(input) shouldEqual output
-//  }
+  "shuffle" should "return correct results" in {
+    val input = List(1,2,3,4,5,6)
+    val output = List(1,4,2,5,3,6)
+
+    shuffle(input) shouldEqual output
+  }
 
   "shuffle" should "return correct result with empty list" in {
     val input = List()
@@ -59,6 +59,62 @@ class CollectionsSpec extends AnyFlatSpec with ScalaCheckDrivenPropertyChecks {
 
     richest(input1) shouldEqual output
     richest(input2) shouldEqual output
+  }
+
+  "greatest" should "return correct result" in {
+    val input1 = List(2,3,5,1,3)
+    val extra1 = 3
+    val output1 = List(true,true,true,false,true)
+
+    greatest(input1, extra1) shouldEqual output1
+
+    val input2 = List(4,2,1,1,2)
+    val extra2 = 1
+    val output2 = List(true,false,false,false,false)
+
+    greatest(input2, extra2) shouldEqual output2
+  }
+
+  "greatest" should "return correct result with empty list" in {
+    val input = List()
+    val extra = 3
+    val output = List()
+
+    greatest(input, extra) shouldEqual output
+  }
+
+  "greatest" should "return correct result with extra equal to zero" in {
+    val input = List(2,3,5,1,3)
+    val extra = 0
+    val output = List(false,false,true,false,false)
+
+    greatest(input, extra) shouldEqual output
+  }
+
+  "widest" should "return correct result" in {
+    val input1 = List((8,7),(9,9),(7,4),(9,7))
+    val output1 = Some(1)
+
+    widest(input1) shouldEqual output1
+
+    val input2 = List((3,1),(9,0),(1,0),(1,4),(5,3),(8,8))
+    val output2 = Some(3)
+
+    widest(input2) shouldEqual output2
+  }
+
+  "widest" should "return correct result with empty list" in {
+    val input = List()
+    val output = None
+
+    widest(input) shouldEqual output
+  }
+
+  "widest" should "return correct result with width equal to zero" in {
+    val input = List((3,1),(3,3))
+    val output = None
+
+    widest(input) shouldEqual output
   }
 
   "findGap" should "find gap" in {
@@ -92,8 +148,8 @@ class CollectionsSpec extends AnyFlatSpec with ScalaCheckDrivenPropertyChecks {
     scanLeft("")(letters)(_ + _) shouldEqual letters.scanLeft("")(_ + _)
     println(scanLeft("")(('a' to 'd').toList.map(_.toString))(_ + _))
   }
-//
-//  "count" should "pass" in {
-//    count("aaaabbbcca") shouldEqual List(('a', 4), ('b', 3), ('c', 2), ('a', 1))
-//  }
+
+  "count" should "pass" in {
+    count("aaaabbbcca") shouldEqual List(('a', 4), ('b', 3), ('c', 2), ('a', 1))
+  }
 }
